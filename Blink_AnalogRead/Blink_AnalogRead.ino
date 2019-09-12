d0f1913d240c417a348a8d639ea04fd8c34a1bf0
LAPTOP-MNMV8UG3\Vulcan <1079021229@qq.com>
2019-09-13 00:01:55 +0800

    弄明白优先级
    记住如果现在进行的操作不想被抢断
    添加taskENTER_CRITICAL();

--- Blink_AnalogRead/Blink_AnalogRead.ino
+++ Blink_AnalogRead/Blink_AnalogRead.ino
@@ -1,107 +1,81 @@
-#include <Arduino_FreeRTOS.h>
+#include "Arduino_FreeRTOS.h"
 
-// define two tasks for Blink & AnalogRead
-void TaskBlink( void *pvParameters );
-void TaskAnalogRead( void *pvParameters );
+void TaskSerial_Print1(void *pvParameters);
+void TaskSerial_Print2(void *pvParameters);
+void TaskBlink(void *pvParameters);
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  
-  // initialize serial communication at 9600 bits per second:
+void setup()
+{
   Serial.begin(9600);
-  
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
-  }
 
-  // Now set up two tasks to run independently.
   xTaskCreate(
     TaskBlink
-    ,  (const portCHAR *)"Blink"   // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL );
+    ,(const portCHAR*)"Blink"
+    ,128
+    ,NULL
+    ,1
+    ,NULL);
 
   xTaskCreate(
-    TaskAnalogRead
-    ,  (const portCHAR *) "AnalogRead"
-    ,  128  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL );
-
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+    TaskSerial_Print1
+    ,(const portCHAR*)"TaskSerial_Print1"
+    ,128
+    ,NULL
+    ,4
+    ,NULL);
+  
+  xTaskCreate(
+    TaskSerial_Print2
+    ,(const portCHAR*)"TaskSerial_Print2"
+    ,128
+    ,NULL
+    ,2
+    ,NULL);
 }
 
 void loop()
 {
-  // Empty. Things are done in Tasks.
+  //taskENTER_CRITICAL();
+  //Serial.print("This is loop! time:");
+  //Serial.println(millis());
+  //taskEXIT_CRITICAL();
+  //delay(1000);
 }
 
-/*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
-/*--------------------------------------------------*/
-
-void TaskBlink(void *pvParameters)  // This is a task.
+void TaskBlink(void *pvParameters)
 {
   (void) pvParameters;
 
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  pinMode(LED_BUILTIN,OUTPUT);
 
-  Most Arduinos have an on-board LED you can control. On the UNO, LEONARDO, MEGA, and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
-  of use the correct LED pin whatever is the board used.
-  
-  The MICRO does not have a LED_BUILTIN available. For the MICRO board please substitute
-  the LED_BUILTIN definition with either LED_BUILTIN_RX or LED_BUILTIN_TX.
-  e.g. pinMode(LED_BUILTIN_RX, OUTPUT); etc.
-  
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-*/
-
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  for (;;) // A Task shall never return or exit.
+  while (1)
   {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    digitalWrite(LED_BUILTIN, HIGH);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+    digitalWrite(LED_BUILTIN, LOW);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 }
 
-void TaskAnalogRead(void *pvParameters)  // This is a task.
+void TaskSerial_Print1(void *pvParameters)
 {
-  (void) pvParameters;
-  
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-*/
-
-  for (;;)
+  vTaskDelay(3000/portTICK_PERIOD_MS);
+  while(1)
   {
-    // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-    // print out the value you read:
-    Serial.println(sensorValue);
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+    Serial.print("This is Print1! time:");
+    Serial.println(millis());
+    vTaskDelay(300/portTICK_PERIOD_MS);
+  }
+}
+
+void TaskSerial_Print2(void *pvParameters)
+{
+  while(1)
+  {
+    taskENTER_CRITICAL();
+    Serial.print("This is Print2! time:");
+    Serial.println(millis());
+    taskEXIT_CRITICAL();
+    vTaskDelay(150/portTICK_PERIOD_MS);
   }
 }
